4ccaff9b043357bf5cdaa83a27fddcdf59120748
Whitney <rioswhit@gmail.com>
2024-07-20 15:27:22 -0400

    converti la columna 'BeginDate' en contract a formato fecha

--- preprocessing/cleaning data.py
+++ preprocessing/cleaning data.py
@@ -1,7 +1,9 @@
+import pandas as pd
 import os, sys
 sys.path.append(os.getcwd())
 
 from funciones.funcion import read_csv
+
 
 #leer los datasets ----------------------------------------------
 user_contract = read_csv('/Users/whitneyrios/PYTHON/My projects py/Proyecto Final 17 Telecom/Proyecto_Final_Telecom/files/datasets/final_provider/contract.csv')
@@ -9,15 +11,17 @@ user_personal_info = read_csv('/Users/whitneyrios/PYTHON/My projects py/Proyecto
 phone_ser = read_csv('/Users/whitneyrios/PYTHON/My projects py/Proyecto Final 17 Telecom/Proyecto_Final_Telecom/files/datasets/final_provider/phone.csv')
 internet_ser = read_csv('/Users/whitneyrios/PYTHON/My projects py/Proyecto Final 17 Telecom/Proyecto_Final_Telecom/files/datasets/final_provider/internet.csv')
 
-#limpiar datos (contratos) ------------------------------------------------
-print(user_contract.head())
+#visualizar datos (contratos) ------------------------------------------------
+user_contract.head(20)
+user_contract.info()
 
-print(user_contract.info())
+#ver valores ausentes y duplicados
+user_contract.isna().sum()
+user_contract.duplicated().sum()
+user_contract['customerID'].duplicated().sum()
+#no hay ausentes ni duplicados 
 
 # TODO : convertir 'BeginDate' a formato fecha
-# TODO: convetir 'TtalCharges a formato float
+# TODO: convetir 'TotalCharges' a formato float
 
-print(user_contract.isna().sum())
-print(user_contract.duplicated().sum())
-print(user_contract['customerID'].duplicated().sum())
-#no hay ausentes ni duplicados 
+user_contract['BeginDate'] = pd.to_datetime(user_contract['BeginDate'], format='%Y-%m-%d')
