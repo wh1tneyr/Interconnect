1cb69eaf8be9885b4268f6bb8985a0f503911dea
Whitney <rioswhit@gmail.com>
2024-07-20 13:54:47 -0400

    leyendo todos los datasets en dataframes

--- preprocessing/cleaning data.py
+++ preprocessing/cleaning data.py
@@ -1,2 +1,10 @@
+import os, sys
+sys.path.append(os.getcwd())
 
+from funciones.funcion import read_csv
 
+#leer los datasets
+user_contract = read_csv('/Users/whitneyrios/PYTHON/My projects py/Proyecto Final 17 Telecom/Proyecto_Final_Telecom/files/datasets/final_provider/contract.csv')
+user_personal_info = read_csv('/Users/whitneyrios/PYTHON/My projects py/Proyecto Final 17 Telecom/Proyecto_Final_Telecom/files/datasets/final_provider/personal.csv')
+phone_ser = read_csv('/Users/whitneyrios/PYTHON/My projects py/Proyecto Final 17 Telecom/Proyecto_Final_Telecom/files/datasets/final_provider/phone.csv')
+internet_ser = read_csv('/Users/whitneyrios/PYTHON/My projects py/Proyecto Final 17 Telecom/Proyecto_Final_Telecom/files/datasets/final_provider/internet.csv')
