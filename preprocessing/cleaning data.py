0aac756c43723fd6852f8845106cae68415ce4b5
Whitney <rioswhit@gmail.com>
2024-07-20 16:00:35 -0400

    converti la columna 'TotalCharges' en contract a float

--- preprocessing/cleaning data.py
+++ preprocessing/cleaning data.py
@@ -15,13 +15,15 @@ internet_ser = read_csv('/Users/whitneyrios/PYTHON/My projects py/Proyecto Final
 user_contract.head(20)
 user_contract.info()
 
-#ver valores ausentes y duplicados
+#ver valores ausentes y duplicados---------------------------------
 user_contract.isna().sum()
 user_contract.duplicated().sum()
 user_contract['customerID'].duplicated().sum()
-#no hay ausentes ni duplicados 
+#no hay ausentes ni duplicados ** 
 
 # TODO : convertir 'BeginDate' a formato fecha
 # TODO: convetir 'TotalCharges' a formato float
 
 user_contract['BeginDate'] = pd.to_datetime(user_contract['BeginDate'], format='%Y-%m-%d')
+user_contract['TotalCharges'] = pd.to_numeric(user_contract['TotalCharges'], errors='coerce')
+ 
