6db5dd0ddfc9975de1c3bd2e1b9631db4b76c291
Whitney <rioswhit@gmail.com>
2024-07-20 16:16:14 -0400

    modificando los comentarios respecto a la limpieza del dataset contract

--- preprocessing/cleaning data.py
+++ preprocessing/cleaning data.py
@@ -5,21 +5,25 @@ sys.path.append(os.getcwd())
 from funciones.funcion import read_csv
 
 
-#leer los datasets ----------------------------------------------
+""" LEER LOS DATOS """
+
 user_contract = read_csv('/Users/whitneyrios/PYTHON/My projects py/Proyecto Final 17 Telecom/Proyecto_Final_Telecom/files/datasets/final_provider/contract.csv')
 user_personal_info = read_csv('/Users/whitneyrios/PYTHON/My projects py/Proyecto Final 17 Telecom/Proyecto_Final_Telecom/files/datasets/final_provider/personal.csv')
 phone_ser = read_csv('/Users/whitneyrios/PYTHON/My projects py/Proyecto Final 17 Telecom/Proyecto_Final_Telecom/files/datasets/final_provider/phone.csv')
 internet_ser = read_csv('/Users/whitneyrios/PYTHON/My projects py/Proyecto Final 17 Telecom/Proyecto_Final_Telecom/files/datasets/final_provider/internet.csv')
 
-#visualizar datos (contratos) ------------------------------------------------
+""" LIMPIAR DATOS (CONTRATOS) """
+
+#visualizar
 user_contract.head(20)
 user_contract.info()
 
-#ver valores ausentes y duplicados---------------------------------
+#ver valores ausentes y duplicados
 user_contract.isna().sum()
 user_contract.duplicated().sum()
 user_contract['customerID'].duplicated().sum()
-#no hay ausentes ni duplicados ** 
+
+""" no hay ausentes ni duplicados  """
 
 # TODO : convertir 'BeginDate' a formato fecha
 # TODO: convetir 'TotalCharges' a formato float
@@ -28,8 +32,7 @@ user_contract['BeginDate'] = pd.to_datetime(user_contract['BeginDate'], format='
 user_contract['TotalCharges'] = pd.to_numeric(user_contract['TotalCharges'], errors='coerce')
  
 #comprobar nuevamente valores ausentes 
-#//user_contract.isna().sum()
+user_contract.isna().sum()
 
+#eliminar valores ausentes 
 user_contract = user_contract.dropna().reset_index(drop=True)
-
-#//user_contract.info()
