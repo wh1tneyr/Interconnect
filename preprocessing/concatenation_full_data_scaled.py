56ffbdf763d190e71da176a071a025dc09b74968
Whitney <rioswhit@gmail.com>
2024-07-21 16:26:59 -0400

    hice un dataframe para todos los datasets escalados y codificados

--- preprocessing/concatenation_full_data_scaled.py
+++ preprocessing/concatenation_full_data_scaled.py
@@ -2,5 +2,11 @@ import pandas as pd
 import os, sys
 sys.path.append(os.getcwd())
 
-""" Leer datos escalados y codificados """
+from funciones.funcion import read_parquet
 
+
+""" Leer datos escalados y codificados """
+user_contract_scaled = read_parquet('files/datasets/data_scaled_encoded/contract_scaled_encoded.parquet')
+user_personal_info_scaled = read_parquet('files/datasets/data_scaled_encoded/personal_scaled_encoded.parquet')
+internet_scaled = read_parquet('files/datasets/data_scaled_encoded/internet_scaled_encoded.parquet')
+phone_scaled = read_parquet('files/datasets/data_scaled_encoded/phone_scaled_encoded.parquet')
