9abd6cd15724ed850a51b19a74665e03a7ba67a3
Whitney <rioswhit@gmail.com>
2024-07-21 16:51:02 -0400

    cambie los indices de todos los datasets a la columna de IDs

--- preprocessing/concatenation_full_data_scaled.py
+++ preprocessing/concatenation_full_data_scaled.py
@@ -6,7 +6,28 @@ from funciones.funcion import read_parquet
 
 
 """ Leer datos escalados y codificados """
+
 user_contract_scaled = read_parquet('files/datasets/data_scaled_encoded/contract_scaled_encoded.parquet')
+#cambiar el indice a la columna de ids
+user_contract_scaled.set_index('customerID', inplace=True)
+
+
+
 user_personal_info_scaled = read_parquet('files/datasets/data_scaled_encoded/personal_scaled_encoded.parquet')
+#cambiar el indice a la columna de ids
+user_personal_info_scaled.set_index('customerID', inplace=True)
+
+
+
 internet_scaled = read_parquet('files/datasets/data_scaled_encoded/internet_scaled_encoded.parquet')
+#cambiar el indice a la columna de ids
+internet_scaled.set_index('customerID', inplace=True)
+
+
+
 phone_scaled = read_parquet('files/datasets/data_scaled_encoded/phone_scaled_encoded.parquet')
+#cambiar el indice a la columna de ids
+phone_scaled.set_index('customerID', inplace=True)
+
+
+""" Concatenar un solo dataframe """
