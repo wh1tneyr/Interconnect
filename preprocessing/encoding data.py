0f1402963e97d967d4a5fd202840d42263cbc911
Whitney <rioswhit@gmail.com>
2024-07-20 21:45:23 -0400

    reduje el codigo en la codificacion de user_contract y la cambie las etiquetas a num enteros

--- preprocessing/encoding data.py
+++ preprocessing/encoding data.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import os, sys
 sys.path.append(os.getcwd())
-from funciones.funcion import encoder
+from funciones.funcion import encoder, scaler
 
 
 """ Codificar user_contract """
 user_contract = pd.read_parquet('files/datasets/final_provider/contract_cleaned.parquet')
-user_contract_to_encode = user_contract[['Type', 'PaperlessBilling', 'PaymentMethod']]
-user_contract_encoded = encoder(user_contract_to_encode)
+user_contract_encoded = encoder(user_contract[['Type', 'PaperlessBilling', 'PaymentMethod']])
+
+#cambiar la codificacion a numeros enteros
+user_contract_encoded = user_contract_encoded.astype('int')
 
