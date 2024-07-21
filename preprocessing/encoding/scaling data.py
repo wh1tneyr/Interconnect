2fe82f169faa888da22dc69a31fe65b9edf99e06
Whitney <rioswhit@gmail.com>
2024-07-20 22:46:27 -0400

    concatene las caracteristicas escaladas y codificadas en user_contract y lo guarde en un archivo parquet

--- preprocessing/encoding/scaling data.py
+++ preprocessing/encoding/scaling data.py
@@ -5,9 +5,22 @@ from funciones.funcion import encoder, scaler
 
 
 """ Codificar user_contract """
+
 user_contract = pd.read_parquet('files/datasets/final_provider/contract_cleaned.parquet')
+user_contract_date_id = user_contract[['customerID', 'BeginDate', 'EndDate']]
+
+
 user_contract_encoded = encoder(user_contract[['Type', 'PaperlessBilling', 'PaymentMethod']])
 
 #cambiar la codificacion a numeros enteros
 user_contract_encoded = user_contract_encoded.astype('int')
 
+""" Escalar user_contract """
+
+user_contract_scaled = scaler(user_contract[['MonthlyCharges', 'TotalCharges']])
+
+
+#oncatenar un solo dataframe user_contract
+user_contract_concat = pd.concat([user_contract_date_id, user_contract_encoded, user_contract_scaled], axis='columns')
+
+user_contract_concat.to_parquet('files/datasets/datasets preprocessed/user_contract_scaled.parquet', engine='pyarrow', index=False)
