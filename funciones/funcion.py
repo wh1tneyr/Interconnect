e5e6b7124eb23f8d00a2aec706ce6b35cea7aac0
Whitney <rioswhit@gmail.com>
2024-07-21 16:21:24 -0400

    cree una funcion para leer archivos parquet

--- funciones/funcion.py
+++ funciones/funcion.py
@@ -1,18 +1,23 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler, OrdinalEncoder
 
+
 """ Funcion para leer archivos csv """
 def read_csv(path):
     data = pd.read_csv(path)
     return data
 
 
+""" Funcion para leer archivos parquet """
+def read_parquet(path):
+    data = pd.read_parquet(path)
+    return data
+
 
 """ Funcion para cambiar a formato fecha """
 def to_date_time(data):
     data = pd.to_datetime(data, format='%Y-%m-%d')
     return data
-
 
 
 """ Funcion para codificar caracteristicas categoricas """
@@ -22,14 +27,11 @@ def encoder(data):
     return data_encoded
     
     
-    
 """ Funcion para escalar caracteristicas numericas """
 def scaler(data):
     scaler = StandardScaler()
     data_scaled = pd.DataFrame(scaler.fit_transform(data), columns=data.columns)
     return data_scaled
-
-
 
 
 """ Funcion para guardar archivos parquet """
