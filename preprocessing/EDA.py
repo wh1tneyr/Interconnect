f9bb125495b84caf898ba6876ed1ef392c30ff13
Whitney <rioswhit@gmail.com>
2024-07-22 22:25:13 -0400

    Evalue el desequilibrio de clases en la variable target

--- preprocessing/EDA.py
+++ preprocessing/EDA.py
@@ -14,12 +14,14 @@ phone_prep = read_csv('files/datasets/final_provider/phone.csv')
 
 """ Examinar la variable target y ver el equilibrio de clases """
 #crear una variable target y pasar los NO a 1 y las fechas a 0
-# 1 = sigue vigente , 0=contrato terminado 
+# 1 = sigue vigente,
+# 0 = contrato terminado
 
 contract_prep['target'] = contract_prep['EndDate']
-
-
 contract_prep['target'] = contract_prep['target'].replace('No', '1').apply(lambda x: 1 if x == '1' else 0)
 
 
-contract_prep
+""" Evaluar el desequilibrio de clases """
+contract_prep['target'].value_counts()
+
+""" """ """ Hay desequilibrio de clases, significa que 1869  personas han terminado su contrato. El resto tiene contrato vigente con la empesa hasta el momento """ """ """
