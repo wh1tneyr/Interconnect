5d85daf7e397a021e8dc174777cb48b0a6574178
Whitney <rioswhit@gmail.com>
2024-07-27 13:14:33 -0400

    cree un nuevo EDA para concatenar todos los datasets

---
+++ preprocessing/EDA_full.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import os, sys
+sys.path.append(os.getcwd())
+
+from funciones.funcion import read_csv, read_parquet
+import matplotlib.pyplot as plt
+
+""" LEER DATOS """
+contract_prep = read_parquet('files/datasets/final_provider/contract_cleaned.parquet') 
+internet_prep = read_csv('files/datasets/final_provider/internet.csv')
+personal_prep = read_csv('files/datasets/final_provider/personal.csv')
+phone_prep = read_csv('files/datasets/final_provider/phone.csv')
+
