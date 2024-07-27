f2be57ec91d7c9e7f1873dc8ee7c7df7c60e6463
Whitney <rioswhit@gmail.com>
2024-07-27 13:47:50 -0400

    concatene toda la info con la info de lineas telefonicas. Rellene los ausentes con 'No'

--- preprocessing/EDA_full.py
+++ preprocessing/EDA_full.py
@@ -11,19 +11,33 @@ internet_prep = read_csv('files/datasets/final_provider/internet.csv')
 personal_prep = read_csv('files/datasets/final_provider/personal.csv')
 phone_prep = read_csv('files/datasets/final_provider/phone.csv')
 
+
+
 # concatenar cotract_prep e internet_prep (sin escalar ni codificar)
 merge_1 = contract_prep.merge(internet_prep, on='customerID', how='left')
 
 # comprobar valores ausentes 
 merge_1.isna().sum()
 
+
 # suponiendo que los ausentes corresponden a clientes que no pagan por servicios de internet,
 # rellenar ausentes con 'No'
-
 merge_1 = merge_1.fillna('No')
 merge_1.isna().sum()
 
+
 # concatenar merge_1 con personal_prep
-
 merge_2 = merge_1.merge(personal_prep, on='customerID', how='left')
+merge_2.isna().sum()
 
+
+# concatenar merge_2 con phone_prep
+full_data = merge_2.merge(phone_prep,  on='customerID', how='left')
+
+full_data.isna().sum()
+
+# suponiendo que los ausentes corresponden a clientes que no tienen multiples lineas,
+# rellenar ausentes con 'No'
+full_data = full_data.fillna('No')
+
+full_data.isna().sum()
