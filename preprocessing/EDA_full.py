313d8ab4edbcbb1e86c8b0e078dde8b34ff43b31
Whitney <rioswhit@gmail.com>
2024-07-27 13:32:07 -0400

    concatene los datos de contract_prep con internet y rellene los valores ausentes

--- preprocessing/EDA_full.py
+++ preprocessing/EDA_full.py
@@ -11,3 +11,14 @@ internet_prep = read_csv('files/datasets/final_provider/internet.csv')
 personal_prep = read_csv('files/datasets/final_provider/personal.csv')
 phone_prep = read_csv('files/datasets/final_provider/phone.csv')
 
+# concatenar todos los conjuntos de datos (sin escalar ni codificar)
+merge_1 = contract_prep.merge(internet_prep, on='customerID', how='left')
+
+# comprobar valores ausentes 
+merge_1.isna().sum()
+
+# suponiendo que los ausentes corresponden a clientes que no pagan por servicios de internet,
+# rellenar ausentes con 'No'
+
+merge_1 = merge_1.fillna('No')
+merge_1.isna().sum()
