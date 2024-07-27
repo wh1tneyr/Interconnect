5eba54048608f3bc4699cb79a8d4efb8cdc6cc48
Whitney <rioswhit@gmail.com>
2024-07-27 16:04:43 -0400

    cree un nuevo df para el promedio de montos mensuales segun tipo de contrato cancelado o vigente

--- preprocessing/EDA_full.py
+++ preprocessing/EDA_full.py
@@ -110,8 +110,38 @@ plt.show()
 """ EVALUAR LOS MONTOS MENSUALES SEGUN TIPO DE CONTRATO """
 
 # agrupar por tipo de contrato  churn y ver el promedio de los cargos mensuales
-monthly_char_type_churn = full_data.groupby(['Type', 'Churn'])['MonthlyCharges'].mean()
+monthly_type_churn = full_data.groupby(['Type', 'Churn'])['MonthlyCharges'].mean()
 
 # reindexar para acceder a los montos 
-monthly_char_type_churn = monthly_char_type_churn.reset_index(name='MonthlyChrges')
+monthly_type_churn = monthly_type_churn.reset_index(name='MonthlyChrges')
 
+# filtrar por contratos cancelados y vigentes
+
+in_ = monthly_type_churn[monthly_type_churn['Churn'] == 1].reset_index(drop=True)
+in_ = in_.drop(['Churn'], axis=1)
+in_.columns = ['Type', 'In']
+
+out_ = monthly_type_churn[monthly_type_churn['Churn'] == 0].reset_index(drop=True)
+out_ = out_.drop(['Churn'], axis=1)
+out_.columns = ['Type', 'Out']
+
+monthly_charges_mean = in_.merge(out_, on='Type')
+
+monthly_charges_mean
+
+
+
+
+# visualizar los montos mensuales
+# crear grafico de barras 
+colors = ['blue', 'orange']
+
+ax_monthly_char_type = monthly_char_type_churn.plot(kind='bar', color=colors)
+ax_monthly_char_type.set_xticklabels(monthly_char_type_churn['Type'], rotation=45)
+
+# Agregar título y etiquetas
+plt.title('Promedio de cargos mensuales segun tipo de contrato y cancelacion')
+plt.xlabel('Tipo de Contrato')
+plt.ylabel('Promedio de cargos mensuales')
+plt.legend(title='Contratos', labels=['In', 'Out'])
+plt.show()
