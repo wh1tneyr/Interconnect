2a884868180c7b03b4764ea188a9528bd4292a9e
Whitney <rioswhit@gmail.com>
2024-07-28 17:11:23 -0400

    lei el df completo, filtre las columnas categoricas, las numericas y las que no necesitan codificacion ni escala

--- preprocessing/encoding_scaling_data.py
+++ preprocessing/encoding_scaling_data.py
@@ -4,4 +4,15 @@ sys.path.append(os.getcwd())
 
 from funciones.funcion import encoder, scaler, read_parquet
 
+
+# leer el dataset completo
 full_data = read_parquet('files/datasets/final_provider/full_data.parquet')
+
+# filtrar las columnas categoricas para codificar
+data_to_encode = full_data[['Type', 'PaperlessBilling', 'PaymentMethod', 'InternetService', 'OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies', 'gender', 'Partner', 'Dependents', 'MultipleLines']]
+
+# filtrar las columnas numericas para escalar
+data_to_scale = full_data[['MonthlyCharges', 'TotalCharges']]
+
+# filtrar las columnas que no necesitan codificacion ni escala
+data_rest = full_data[['customerID', 'BeginDate', 'EndDate', 'SeniorCitizen', 'Churn']]
