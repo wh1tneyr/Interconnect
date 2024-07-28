e8b0de0adf1c429c41f69bfe486aa0c11a6c003e
Whitney <rioswhit@gmail.com>
2024-07-28 17:21:39 -0400

    codifique las variables categoricas y le agregue los IDs

--- preprocessing/encoding_scaling_data.py
+++ preprocessing/encoding_scaling_data.py
@@ -9,10 +9,17 @@ from funciones.funcion import encoder, scaler, read_parquet
 full_data = read_parquet('files/datasets/final_provider/full_data.parquet')
 
 # filtrar las columnas categoricas para codificar
-data_to_encode = full_data[['Type', 'PaperlessBilling', 'PaymentMethod', 'InternetService', 'OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies', 'gender', 'Partner', 'Dependents', 'MultipleLines']]
+data_to_encode = full_data[['customerID', 'Type', 'PaperlessBilling', 'PaymentMethod', 'InternetService', 'OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies', 'gender', 'Partner', 'Dependents', 'MultipleLines']]
 
 # filtrar las columnas numericas para escalar
 data_to_scale = full_data[['MonthlyCharges', 'TotalCharges']]
 
 # filtrar las columnas que no necesitan codificacion ni escala
 data_rest = full_data[['customerID', 'BeginDate', 'EndDate', 'SeniorCitizen', 'Churn']]
+
+
+# codificar las variables categoricas
+data_encoded = encoder(data_to_encode)
+
+# agregar columna de IDs
+data_encoded['customerID'] = full_data['customerID']
