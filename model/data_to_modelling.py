a2fc1e79b28f5587972168ed4e4dfb8c292614db
Whitney <rioswhit@gmail.com>
2024-07-31 22:31:03 -0400

    hice un df con el dataset completo escalado y codificado

--- model/data_to_modelling.py
+++ model/data_to_modelling.py
@@ -2,6 +2,9 @@ import pandas as pd
 import os, sys
 sys.path.append(os.getcwd())
 
-from funciones.funcion import read_parquet, read_csv
+from funciones.funcion import read_parquet
 
-df = read_parquet('')
+
+# leer el dataset completo escalado y codificado
+data = read_parquet('files/datasets/final_provider/full_data_scaled.parquet')
+
