0620b1c7beb272ed20716c93bac012285c571d8e
Whitney <rioswhit@gmail.com>
2024-08-05 22:43:23 -0400

    hice un dataset para el modelo y quite las columnas de cargos totales y fecha de cancelacion

--- model/data_to_modelling.py
+++ model/data_to_modelling.py
@@ -8,3 +8,6 @@ from funciones.funcion import read_parquet
 # leer el dataset completo escalado y codificado
 data = read_parquet('files/datasets/final_provider/full_data_scaled.parquet')
 
+# quitar las columnas de cargos totales y fecha de cancelacion
+data_to_model = data.drop(['EndDate', 'TotalCharges'], axis=1)
+
