c76909684e5ad1570597ffca24512eeb558d69f2
Whitney <rioswhit@gmail.com>
2024-08-05 23:36:38 -0400

    dividi tres subconjuntos de datos: de entrenamiento, prueba y validacion

--- model/data_to_modelling.py
+++ model/data_to_modelling.py
@@ -2,7 +2,8 @@ import pandas as pd
 import os, sys
 sys.path.append(os.getcwd())
 
-from funciones.funcion import read_parquet
+from funciones.funcion import read_parquet, parquet
+from sklearn.model_selection import train_test_split
 
 
 # leer el dataset completo escalado y codificado
@@ -10,4 +11,12 @@ data = read_parquet('files/datasets/final_provider/full_data_scaled.parquet')
 
 # quitar las columnas de cargos totales y fecha de cancelacion
 data_to_model = data.drop(['EndDate', 'TotalCharges'], axis=1)
+
+
+# dividir subconjuntos de entrenamiento, prueba y validación
+
+data_train, data_test = train_test_split(data_to_model, test_size=0.30, random_state=42)
+
+data_valid, data_valid_2 = train_test_split(data_test, test_size=0.30, random_state=42)
+
 
