1bf356b36f8494d71d9a9f7351b59a441fd4d141
Whitney <rioswhit@gmail.com>
2024-08-12 21:18:46 -0400

    Separe los nombres de las columnas segun si son categoricas o numericas para codificar y escalar

--- preprocessing/train_test_scaling_encoding.py
+++ preprocessing/train_test_scaling_encoding.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os, sys
 sys.path.append(os.getcwd())
 
-from funciones.funcion import read_parquet, parquet
+from funciones.funcion import read_parquet, parquet, scaler_train, scaler_test, encoder_train, encoder_test
 from sklearn.model_selection import train_test_split
 
 
@@ -10,3 +10,16 @@ from sklearn.model_selection import train_test_split
 data_train = read_parquet('files/datasets/final_provider/data_train.parquet')
 data_test = read_parquet('files/datasets/final_provider/data_test.parquet')
 data_valid = read_parquet('files/datasets/final_provider/data_valid.parquet')
+
+
+data_train.info()
+
+# Codificar varibales categoricas en subconjuntos 
+
+# # Filtrar los nombres de columnas segun el tipo 
+
+cat_columns = ['customerID', 'Type', 'PaperlessBilling', 'PaymentMethod', 'InternetService', 'OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies', 'gender', 'Partner', 'Dependents', 'MultipleLines']
+
+num_columns = ['MonthlyCharges', 'TotalCharges']
+
+other_columns = ['customerID', 'BeginDate', 'EndDate', 'SeniorCitizen', 'Churn']
