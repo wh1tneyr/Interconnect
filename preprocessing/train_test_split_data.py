9dd47b5eec94495e4db2eec536c3abb8b7b3dcc9
Whitney <rioswhit@gmail.com>
2024-08-12 20:35:11 -0400

    hice una variable para segmentar los subconjuntos y elimine las columnas de fechas

---
+++ preprocessing/train_test_split_data.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import os, sys
+sys.path.append(os.getcwd())
+
+from funciones.funcion import read_parquet, parquet
+from sklearn.model_selection import train_test_split
+
+
+# Leer el dataset completo original
+full_data = pd.read_parquet('files/datasets/final_provider/full_data.parquet')
+
+
+# Eliminar las columnas de fechas del df 
+data_to_split = full_data.drop(['EndDate', 'BeginDate'], axis=1)
